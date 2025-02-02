bf0412c6c5708457a7fb54cd99b17dbb3cc8e013
mon_sat <tnk@daijiten.info>
2025-02-02 11:30:40 +0900

    feat(base): route rules proxty

---
+++ packages/base/app/pages/proxy.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const headers = {
+  'x-coedo-name': 'kayabacho',
+}
+
+const {
+  data,
+  status,
+} = useFetch('/api/proxy/foo/bar', {
+  query: {
+    path: '/api/foo/bar',
+    q: 'search-string',
+  },
+  headers,
+})
+
+const results = ref<Record<string, unknown>>({})
+const post = async () => {
+  const fetched = await $fetch<Record<string, unknown>>('/api/proxy/foo/bar', {
+    query: {
+      path: '/api/foo/bar/baz',
+    },
+    headers,
+  })
+    .catch((err) => {
+      console.log('fetch error:', err)
+    })
+  if (fetched != null) {
+    results.value = fetched
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>useFetch: Proxy</h1>
+    <div>
+      <button @click="post">$fetch</button>
+    </div>
+    <pre v-if="status === 'success'">
+      {{ data }}
+    </pre>
+    <hr>
+    <pre>
+      {{ results }}
+    </pre>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- packages/base/nuxt.config.ts
+++ packages/base/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    '/api/proxy/**': {
+      proxy: 'http://localhost:3300/api/**',
+    },
+  },
   future: {
     compatibilityVersion: 4,
   },
